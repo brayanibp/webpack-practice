bbee14f5f30ffd898035c0b8183a2c48c30b8a71
Brayan Bernal <brayanbernal0710@gmail.com>
2020-01-15 13:18:22 -0400

    Empezando clase de URL Loader

---
+++ url-loader/src/js/components/app.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import data from './data.json.js';
+import Loader from './loader';
+
+console.log(data);
+function App() {
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
+    return (
+        <div>
+            <p>Que bonito es lo bonito hecho en react</p>
+            <ul>
+                {
+                    loaderList.map(item => <Loader {...item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}>Mostrar todo lo aprendido hasta el momento</button>
+        </div>
+    );
+}
+
+export default App;
