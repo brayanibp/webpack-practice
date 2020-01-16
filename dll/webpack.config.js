8269c28320347a2b77760694ff00496c53c73db1
Brayan Bernal <brayanbernal0710@gmail.com>
2020-01-16 16:46:11 -0400

    Guardando cambios de clase para añadir un DynamicLink Library (DLL o dll)

---
+++ dll/webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname,'src/js/index.js'),
+        contacto: path.resolve(__dirname,'src/js/contacto.js')
+    },
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/[name].js'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 90000
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Babel',
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ]
+}
