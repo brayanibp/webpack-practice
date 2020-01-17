6feea4a54d0bfba6201ded922aa3716c3bae24e3
Brayan Bernal <brayanbernal0710@gmail.com>
2020-01-17 10:01:41 -0400

    Agregando carpeta dynamic-imports antes de empezar clase de Dynamic Imports en platzi

---
+++ dynamic-imports/src/js/components/app.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader';
+import logo from '../../images/platzi.png';
+import video from '../../video/que-es-core.mp4';
+
+import '../../less/less.less';
+import '../../sass/sass.scss';
+import '../../stylus/stylus.styl';
+
+console.log(data);
+function App() {
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
+    //<video src={video} with={360} controls poster={logo}></video>
+    return (
+        <div>
+            <p className="sass">Esto es sass</p>
+            <p className="less">Esto es less</p>
+            <p className="stylus">Esto es stylus</p>
+            <p className="post-css">Esto es postcss</p>
+            <p>Que bonito es lo bonito hecho en react</p>
+            <div>
+                Vacio
+                <video src={video} with={360} controls poster={logo}></video>
+            </div>
+            <div>
+                <img src={logo} alt="" width={40} />
+            </div>
+            <ul>
+                {
+                    loaderList.map(item => <Loader {...item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}>Mostrar todo lo aprendido hasta el momento</button>
+        </div>
+    );
+}
+
+export default App;
